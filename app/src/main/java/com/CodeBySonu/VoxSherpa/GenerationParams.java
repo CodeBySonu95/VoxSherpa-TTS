9bb07959baf94762209e9b705c94cd75a9577676
Sonu Singh <codebysonu95@gmail.com>
2026-07-22 20:56:31 +0530

    Update GenerationParams.java

--- app/src/main/java/com/CodeBySonu/VoxSherpa/GenerationParams.java
+++ app/src/main/java/com/CodeBySonu/VoxSherpa/GenerationParams.java
@@ -1,19 +1,21 @@
 package com.CodeBySonu.VoxSherpa;
 
 public class GenerationParams {
-    public final String text, onnxModel, tokens, modelType, voicesBin;
+    // Added language to the string parameters
+    public final String text, onnxModel, tokens, modelType, voicesBin, language;
     public final int kokoroVoiceId;
     public final float speed, pitch;
     public final boolean punctOn, emotionOn;
 
     public GenerationParams(String text, String onnxModel, String tokens,
-                     String modelType, String voicesBin, int kokoroVoiceId,
+                     String modelType, String voicesBin, String language, int kokoroVoiceId,
                      float speed, float pitch, boolean punctOn, boolean emotionOn) {
         this.text = text;
         this.onnxModel = onnxModel;
         this.tokens = tokens;
         this.modelType = modelType;
         this.voicesBin = voicesBin;
+        this.language = language; // Store the language properly
         this.kokoroVoiceId = kokoroVoiceId;
         this.speed = speed;
         this.pitch = pitch;
